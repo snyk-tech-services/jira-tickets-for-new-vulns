c9e1e44f103573101f143e8f9d8dce56b0fef2d2
Mathilde <mathilde.riou@snyk.io>
2022-01-14 17:48:04 +0000

    minor: filter issue type to process only vuln and license issue types

--- vulns_test.go
+++ vulns_test.go
@@ -9,11 +9,6 @@ import (
 // Test getVulnsWithoutTicket function
 func TestGetVulnsWithoutTicketFunc(t *testing.T) {
 
-	//getVulnsWithoutTicket(endpointAPI string, orgID string, projectID string, token string, severity string, issueType string, tickets map[string]string) []interface{} {
-	//return []interface, array of vulns
-	// get http server to return projectIssuesPerPath.json like API would
-	// compare []interface with projectVulnsPerPath.json
-
 	assert := assert.New(t)
 
 	server := HTTPResponseCheckAndStub_()
@@ -60,6 +55,61 @@ func TestGetVulnsWithoutTicketFunc(t *testing.T) {
 	response := getVulnsWithoutTicket(flags, "123", maturityLevels, tickets, cD)
 	//fmt.Println(response)
 	assert.Equal(2, len(response))
+
+	return
+}
+
+// Test that we ignore any issue that is not of type license or a vuln
+// only vuln and license can be in the same aggregated issue reponse atm
+// IAC are separated projects with only configuration type
+// snyk code is in a separated API atm
+
+func TestNoVulnOrLicense(t *testing.T) {
+
+	assert := assert.New(t)
+
+	server := HTTPResponseCheckAndStubNoVulnOrLicense()
+
+	defer server.Close()
+
+	// setting mandatory options
+	Mf := MandatoryFlags{}
+	Mf.orgID = "123"
+	Mf.endpointAPI = server.URL
+	Mf.apiToken = "123"
+	Mf.jiraProjectID = "123"
+	Mf.jiraProjectKey = ""
+
+	// setting optional options
+	Of := optionalFlags{}
+	Of.severity = "low"
+	Of.priorityScoreThreshold = 0
+	Of.issueType = "all"
+	Of.debug = false
+	Of.jiraTicketType = "Bug"
+	Of.assigneeID = ""
+	Of.assigneeName = ""
+	Of.labels = ""
+	Of.priorityIsSeverity = false
+	Of.projectID = ""
+	Of.maturityFilterString = ""
+	Of.ifUpgradeAvailableOnly = false
+
+	flags := flags{}
+	flags.mandatoryFlags = Mf
+	flags.optionalFlags = Of
+
+	// setting debug
+	cD := debug{}
+	cD.setDebug(false)
+
+	var tickets map[string]string
+	tickets = make(map[string]string)
+	var maturityLevels []string
+
+	response := getVulnsWithoutTicket(flags, "123", maturityLevels, tickets, cD)
+	//fmt.Println(response)
+	assert.Equal(0, len(response))
 
 	return
 }
