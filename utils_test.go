b0484621bf009706b7e5df2d77e690dfd371fb83
ghe <lili@snyk.io>
2022-11-02 23:32:32 +0000

    minor: filter out Code Issues based on prioriityScore if set

--- utils_test.go
+++ utils_test.go
@@ -498,35 +498,35 @@ func HTTPResponseCodeIssueStubAndMirrorRequest() *httptest.Server {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeDataEmpty.json")
 		} else if r.RequestURI == "/v3/orgs/123/issues?project_id=7891&severity=medium&version=2021-08-20~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeDataMediumIssues.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00d?project_id=456&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00d?project_id=456&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetails.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00d?project_id=1234&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00d?project_id=1234&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetails.json")
-		} else if r.RequestURI == "/v3/orgs/xxx99a85-c519-xxxx-ae55-xxx9b9bfaxxx/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00q?project_id=1234&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/xxx99a85-c519-xxxx-ae55-xxx9b9bfaxxx/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00q?project_id=1234&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetailsForPagination.json")
-		} else if r.RequestURI == "/v3/orgs/xxx99a85-c519-xxxx-ae55-xxx9b9bfaxxx/issues/detail/code/xxbac5ed-critical2-xx65-8730-2xxx4467e00q?project_id=1234&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/xxx99a85-c519-xxxx-ae55-xxx9b9bfaxxx/issues/detail/code/xxbac5ed-critical2-xx65-8730-2xxx4467e00q?project_id=1234&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetailsForPagination.json")
-		} else if r.RequestURI == "/v3/orgs/xxx99a85-c519-xxxx-ae55-xxx9b9bfaxxx/issues/detail/code/xxbac5ed-critical1-xx65-8730-2xxx4467e00q?project_id=1234&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/xxx99a85-c519-xxxx-ae55-xxx9b9bfaxxx/issues/detail/code/xxbac5ed-critical1-xx65-8730-2xxx4467e00q?project_id=1234&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetailsForPagination.json")
-		} else if r.RequestURI == "/v3/orgs/xxx99a85-c519-xxxx-ae55-xxx9b9bfaxxx/issues/detail/code/bbbbbbb-83dd-xx65-8730-2xxx4467e00q?project_id=1234&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/xxx99a85-c519-xxxx-ae55-xxx9b9bfaxxx/issues/detail/code/bbbbbbb-83dd-xx65-8730-2xxx4467e00q?project_id=1234&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetailsForPagination.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0xx?project_id=456&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0xx?project_id=456&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetails2.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0zz?project_id=456&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0zz?project_id=456&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetails3.json")
 		} else if r.RequestURI == "/v1/org/123/project/456/issue/SNYK-JS-MINIMIST-559764/jira-issue" {
 			resp = readFixture("./fixtures/singleJiraTicketOpeningResponse.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00d?project_id=789&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00d?project_id=789&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetails.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0xx?project_id=789&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0xx?project_id=789&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetails2.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0zz?project_id=789&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0zz?project_id=789&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetails3.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00d?project_id=7891&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e00d?project_id=7891&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetailsWithIgnore.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0xx?project_id=7891&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0xx?project_id=7891&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetails2.json")
-		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0zz?project_id=7891&version=2021-08-20~experimental" {
+		} else if r.RequestURI == "/v3/orgs/123/issues/detail/code/xxbac5ed-83dd-xx65-8730-2xxx4467e0zz?project_id=7891&version=2022-04-06~experimental" {
 			resp = readFixture("./fixtures/snyk_code_fixtures/snykCodeIssueDetails3.json")
 		} else {
 			fmt.Println("**** TEST ERROR *** Could not match URL", r.RequestURI)
