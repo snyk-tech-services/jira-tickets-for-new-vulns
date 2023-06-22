9594f606d00a24279b9fd591f072a30e1d7ae1b0
Lili Kastilio <lili@snyk.io>
2023-06-22 11:19:46 +0100

    major: Migrate to REST List Projects API & improvements
    major: Migrate to REST List Projects API
    
    This is to address https://github.com/snyk-tech-services/jira-tickets-for-new-vulns/issues/182
    
    Co-authored-by: Troy Havelock <troy.havelock@snyk.io>
    
    * minor: Added DueDate support for creating Jira (#185)
    
    Added the optional flag for the due date built-in flag.
    Updated README with instructions
    
    Co-authored-by: Alexandre Vallières-Lagacé <alexandre@circleci.com>
    
    * minor: align urls with the recommended domains and paths (#192)
    
    Switch from:
    - `snyk.io/api` to `api.snyk.io`
    - `api.snyk.io/api` to `api.snyk.io`
    - `snyk.io/vuln` to `security.snyk.io/vuln`
    - `api.snyk.io/v3` to `api.snyk.io/rest`
    
    * minor: use public utility container for github releasing tool (#194)
    * major: Migrate to REST List Projects API (#199)
    
    
    ---------
    Co-authored-by: Jeff B <jeff.b@runbox.com>
    Co-authored-by: Troy Havelock <troy.havelock@snyk.io>
    Co-authored-by: Alexandre Vallières-Lagacé <alexandre@circleci.com>
    Co-authored-by: Jérémy Lourenço <jeremy.lourenco@snyk.io>
    Co-authored-by: aarlaud <antoine@snyk.io>
    Co-authored-by: Jeff B <jeff.b@runbox.com>

--- snyk.go
+++ snyk.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -10,75 +9,31 @@ import (
 	"github.com/michael-go/go-jsn/jsn"
 )
 
-// ProjectsFilter is the top level filter type of filtering Snyk project
-type ProjectsFilter struct {
-	Filters ProjectsFilterBody `json:"filters"`
-}
+func getOrgProjects(flags flags, customDebug debug) ([]jsn.Json, error) {
+	verb := "GET"
+	api_version := "2022-07-08~beta"
 
-type ProjectsFilterBody struct {
-	Attributes *ProjectsFiltersAttributes `json:"attributes,omitempty"`
-	Monitored  bool                       `json:"isMonitored"`
-}
+	baseURL := flags.mandatoryFlags.endpointAPI + "/rest"
 
-type ProjectsFiltersAttributes struct {
-	Criticality []string `json:"criticality,omitempty"`
-	Environment []string `json:"environment,omitempty"`
-	Lifecycle   []string `json:"lifecycle,omitempty"`
-}
-
-func getOrgProjects(flags flags, customDebug debug) (jsn.Json, error) {
-	// According to https://snyk.docs.apiary.io/#reference/projects/all-projects/list-all-projects this should be
-	// a POST API call but historically we used GET here. The following code maintains backwards compatibility for
-	// existing cases where people aren't filtering projects by attributes, as it appears the API does not return
-	// the full project list with empty attribute filters in the request body.
-	verb := "POST"
-	// filter out inactive projects
-	// filter out inactive projects
-	projectFilters := &ProjectsFilterBody{
-		Monitored: true,
-	}
-	projectsAPI := flags.mandatoryFlags.endpointAPI + "/v1/org/" + flags.mandatoryFlags.orgID + "/projects"
+	projectsAPI := "/orgs/" + flags.mandatoryFlags.orgID + "/projects?version=" + api_version + "&status=active&limit=100"
 	if len(flags.optionalFlags.projectCriticality) > 0 || len(flags.optionalFlags.projectEnvironment) > 0 || len(flags.optionalFlags.projectLifecycle) > 0 {
-		attributes := &ProjectsFiltersAttributes{}
 
 		if len(flags.optionalFlags.projectCriticality) > 0 {
-			attributes.Criticality = strings.Split(flags.optionalFlags.projectCriticality, ",")
+			projectsAPI += "&businessCriticality=" + strings.Replace(flags.optionalFlags.projectCriticality, ",", "%2C", -1)
 		}
 
 		if len(flags.optionalFlags.projectEnvironment) > 0 {
-			attributes.Environment = strings.Split(flags.optionalFlags.projectEnvironment, ",")
+			projectsAPI += "&environment=" + strings.Replace(flags.optionalFlags.projectEnvironment, ",", "%2C", -1)
 		}
 
 		if len(flags.optionalFlags.projectLifecycle) > 0 {
-			attributes.Lifecycle = strings.Split(flags.optionalFlags.projectLifecycle, ",")
+			projectsAPI += "&lifecycle=" + strings.Replace(flags.optionalFlags.projectLifecycle, ",", "%2C", -1)
 		}
-
-		// filter out inactive projects
-		projectFilters.Attributes = attributes
-
 	}
 
-	body := &ProjectsFilter{
-		Filters: *projectFilters,
-	}
-
-	var marshalledBody []byte
 	var err error
 
-	if verb == "POST" {
-		marshalledBody, err = json.Marshal(body)
-		if err != nil {
-			// FIXME: don't fall back, we should error instead
-			log.Printf("*** ERROR *** Not able to read attributes %s\n", projectsAPI)
-			errorMessage := fmt.Sprintf("Failure, Not able to read attributes\n")
-			writeErrorFile("getOrgProjects", errorMessage, customDebug)
-			err = errors.New(errorMessage)
-			verb = "GET"
-			marshalledBody = nil
-		}
-	}
-
-	responseData, err := makeSnykAPIRequest(verb, projectsAPI, flags.mandatoryFlags.apiToken, marshalledBody, customDebug)
+	projectList, err := makeSnykAPIRequest_REST(verb, baseURL, projectsAPI, flags.mandatoryFlags.apiToken, nil, customDebug)
 	if err != nil {
 		filters := "projectCriticality: " + flags.optionalFlags.projectCriticality + "\n projectEnvironment: " + flags.optionalFlags.projectEnvironment + "\n projectLifecycle: " + flags.optionalFlags.projectLifecycle
 		log.Printf("*** ERROR *** Could not list the Project(s) for endpoint %s\n Applied Filters: %s\n", projectsAPI, filters)
@@ -87,20 +42,12 @@ func getOrgProjects(flags flags, customDebug debug) (jsn.Json, error) {
 		err = errors.New(errorMessage)
 	}
 
-	project, err := jsn.NewJson(responseData)
-	if err != nil {
-		log.Printf("*** ERROR *** Could not get read the response from endpoint %s\n", projectsAPI)
-		errorMessage := fmt.Sprintf("Failure, Could not get read the response from endpoint %s ", projectsAPI)
-		writeErrorFile("getOrgProjects", errorMessage, customDebug)
-		err = errors.New(errorMessage)
-	}
-
-	return project, err
+	return projectList, err
 }
 
 func getProjectsIds(options flags, customDebug debug, notCreatedLogFile string) ([]string, error) {
 
-	var projectId []string
+	var projectIds []string
 	if len(options.optionalFlags.projectID) == 0 {
 		filters := "projectCriticality: " + options.optionalFlags.projectCriticality + "\n projectEnvironment: " + options.optionalFlags.projectEnvironment + "\n projectLifecycle: " + options.optionalFlags.projectLifecycle
 		log.Println("*** INFO *** Project ID not specified - listing all projects that match the following filters: ", filters)
@@ -111,23 +58,23 @@ func getProjectsIds(options flags, customDebug debug, notCreatedLogFile string) 
 			writeErrorFile("getProjectsIds", message, customDebug)
 			return nil, err
 		}
-
-		for i := 0; i < len(projects.K("projects").Array().Elements()); i++ {
-			p := projects.K("projects").Array().Elements()[i]
-			projectId = append(projectId, string(p.K("id").String().Value))
+	
+		for _, project := range projects {
+			projectID := project.K("id").String().Value
+			projectIds = append(projectIds, projectID)
 		}
 
-		if len(projectId) == 0 {
+		if len(projectIds) == 0 {
 			ErrorMessage := fmt.Sprintf("Failure, Could not retrieve project ID")
 			writeErrorFile("getProjectsIds", ErrorMessage, customDebug)
-			return projectId, errors.New(ErrorMessage)
+			return projectIds, errors.New(ErrorMessage)
 		}
-		return projectId, nil
+		return projectIds, nil
 	}
 
-	projectId = append(projectId, options.optionalFlags.projectID)
+	projectIds = append(projectIds, options.optionalFlags.projectID)
 
-	return projectId, nil
+	return projectIds, nil
 }
 
 func getProjectDetails(Mf MandatoryFlags, projectID string, customDebug debug) (jsn.Json, error) {
--- snyk_utils.go
+++ snyk_utils.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/michael-go/go-jsn/jsn"
 )
 
 func makeSnykAPIRequest(verb string, endpointURL string, snykToken string, body []byte, customDebug debug) ([]byte, error) {
@@ -130,4 +132,91 @@ func makeSnykAPIRequest(verb string, endpointURL string, snykToken string, body 
 	}
 	time.Sleep(1)
 	return responseData, err
+}
+
+
+// This does not have general testing or capabilities at present.
+// So far, is being tested only with GET https://api.snyk.io/rest/orgs/[OrgID]/projects
+// Need to investigate and update error handling TODO
+// currently no retry logic TODO
+func makeSnykAPIRequest_REST(verb string, baseURL string, endpointURL string, snykToken string, body []byte, customDebug debug) ([]jsn.Json, error) {
+	var err error
+	allData := []jsn.Json{}
+	bodyBuffer := bytes.NewBuffer(nil)
+
+	url := baseURL + endpointURL
+	client := &http.Client{}
+
+	for url != "" { 
+		if verb == "POST" && body != nil {
+			bodyBuffer = bytes.NewBuffer(body)
+		}
+
+		request, err := http.NewRequest(verb, url, bodyBuffer)
+		if err != nil {
+			customDebug.Debugf("*** ERROR *** could not create requests to '%s' failed with error %s\n", url, err.Error())
+			return []jsn.Json{}, err
+		}
+	
+		request.Header.Add("Accept", "application/vnd.api+json")
+		request.Header.Add("Authorization", snykToken)
+		request.Header.Set("User-Agent", "tech-services/snyk-jira-tickets-for-new-vulns")
+
+		response, err := client.Do(request)
+		if err != nil {
+			customDebug.Debugf("*** ERROR *** Request on endpoint '%s' failed with error %s\n", url, err.Error())
+			return []jsn.Json{}, err
+		}
+		defer response.Body.Close()
+    	// fmt.Println("Response Status Code:", response.StatusCode)
+
+		customDebug.Debugf("*** INFO *** Sending %s request to %s", verb, url)
+
+		jsonResponse, err := jsn.NewJson(response.Body)
+		if err != nil {
+			customDebug.Debugf("*** ERROR *** failed to load load json from response from endpoint %s with error %s\n", url, err.Error())
+		}
+
+		if response.StatusCode == 404 {
+			customDebug.Debugf("*** INFO *** Request on endpoint '%s' failed with error %s\n", endpointURL, response.Status)
+			errorMessage := fmt.Sprintf("*** INFO *** Request on endpoint '%s' failed with error %s\n", endpointURL, response.Status)
+			writeErrorFile("makeSnykAPIRequest", errorMessage, customDebug)
+			return nil, errors.New("Not found, Request failed")
+		} else if response.StatusCode == 400 {
+			customDebug.Debugf("*** INFO *** Request on endpoint '%s' failed with error %s\n", endpointURL, response.Status)
+			customDebug.Debugf("*** INFO *** Please check that all expected fields are present in the config file\n")
+			customDebug.Debugf("*** INFO *** Details : %s\n", jsonResponse.Pretty())
+			errorMessage := fmt.Sprintf("*** INFO *** Request on endpoint '%s' failed with error %s\n", endpointURL, response.Status)
+			writeErrorFile("makeSnykAPIRequest", errorMessage, customDebug)
+			return nil, errors.New("Unprocessable Entity, Request failed")
+		} else if response.StatusCode == 401 || response.StatusCode == 403 {
+			customDebug.Debugf("*** INFO *** Request on endpoint '%s' failed with error %s\n", endpointURL, response.Status)
+			customDebug.Debugf("*** INFO *** Please valid API token and permissions\n")
+			customDebug.Debugf("*** INFO *** Details : %s\n", jsonResponse.Pretty())
+			errorMessage := fmt.Sprintf("*** INFO *** Request on endpoint '%s' failed with error %s\n", endpointURL, response.Status)
+			writeErrorFile("makeSnykAPIRequest", errorMessage, customDebug)
+			return nil, errors.New("Authentication or permission error, Request failed")
+		} else if response.StatusCode > 400 {
+			customDebug.Debugf("*** INFO *** Request on endpoint '%s' failed with error %s\n", endpointURL, response.Status)
+			customDebug.Debugf("*** INFO *** Please check that all expected fields are present in the config file\n")
+			customDebug.Debugf("*** INFO *** Details : %s\n", jsonResponse.Pretty())
+			errorMessage := fmt.Sprintf("*** INFO *** Request on endpoint '%s' failed with error %s\n", endpointURL, response.Status)
+			writeErrorFile("makeSnykAPIRequest", errorMessage, customDebug)
+			return nil, errors.New("Request failed")
+		}
+
+		data := jsonResponse.K("data").Array()
+		allData = append(allData, data.Elements()...)
+
+		// Check if there is a next link, if not empty string ends the loop
+		next := jsonResponse.K("links").K("next").String()
+		if !next.IsValid {
+			url = ""
+		} else {
+			url = baseURL + next.Value
+		}
+	}
+
+	time.Sleep(1)
+	return allData, err
 }
